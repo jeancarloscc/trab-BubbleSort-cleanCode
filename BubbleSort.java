27eda6917a354331971b2cf49379044904f3f8b6
jeancarlos <jeancc.costa@gmail.com>
2024-07-12 10:49:59 -0300

    inserção do BubbleSort

---
+++ BubbleSort.java
@@ -0,0 +1,54 @@
+// Implementação otimizada em Java do Bubble sort
+// Código extraído de https://www.geeksforgeeks.org/bubble-sort/
+
+import java.io.*;
+
+class BubbleSort {
+
+    // An optimized version of Bubble Sort
+    static void bubbleSort(int arr[], int n)
+    {
+        int i, j, temp;
+        boolean trocado;
+        for (i = 0; i < n - 1; i++) {
+            trocado = false;
+            for (j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+
+                    // Swap arr[j] and arr[j+1]
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    trocado = true;
+                }
+            }
+
+            // If no two elements were
+            // trocado by inner loop, then break
+            if (trocado == false)
+                break;
+        }
+    }
+
+    // Function to print an array
+    static void printArray(int arr[], int size)
+    {
+        int i;
+        for (i = 0; i < size; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    // Driver program
+    public static void main(String args[])
+    {
+        int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+        int n = arr.length;
+        bubbleSort(arr, n);
+        System.out.println("Array ordenado: ");
+        printArray(arr, n);
+    }
+}
+
+// This code is contributed
+// by Nikita Tiwari.
---
+++ out/production/trab-BubbleSort-cleanCode/README.md
@@ -0,0 +1,2 @@
+# Bubble Sort para ser mais Clean Code
+Este é um trabalho para melhorar o código do Bubble Sort utilizando o conhecimento adquirido da disciplina de Aspectos Ágeis de Programação no curso de Especialização em Desenvolvimento Ágil de Software ofertado pela UFPR.
